9b47aff0e3a9f6a87ad7c98af7ecc466009d465d
akrango <angelakrango27@gmail.com>
2024-05-10 19:46:16 +0200

    added driver and desired capabilities

--- src/main/java/org/example/constants/AppConstants.java
+++ src/main/java/org/example/constants/AppConstants.java
@@ -1,4 +1,9 @@
 package org.example.constants;
 
 public class AppConstants {
+    public static final String PLATFORM_NAME="Android";
+    public static final String DEVICE_NAME="emulator-5554";
+    public static final String APP_PACKAGE="com.android.contacts";
+    public static final String APP_ACTIVITY="com.android.contacts.activities.PeopleActivity";
+    public static final String AUTOMATION_NAME="UiAutomator2";
 }
--- src/main/java/org/example/utils/DriverUtils.java
+++ src/main/java/org/example/utils/DriverUtils.java
@@ -1,4 +1,43 @@
 package org.example.utils;
 
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static org.example.constants.AppConstants.*;
+
 public class DriverUtils {
+    private static AppiumDriver driver;
+
+    public static DesiredCapabilities getDesiredCapabilities() {
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("platformName", PLATFORM_NAME);
+        desiredCapabilities.setCapability("deviceName", DEVICE_NAME);
+        desiredCapabilities.setCapability("appPackage", APP_PACKAGE);
+        desiredCapabilities.setCapability("appActivity", APP_ACTIVITY);
+        desiredCapabilities.setCapability("automationName", AUTOMATION_NAME);
+        return desiredCapabilities;
+    }
+    public static void initDriver() {
+
+        try {
+            driver = new AndroidDriver(new URL("http://localhost:4723"), getDesiredCapabilities());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static AppiumDriver getDriver() {
+        return driver;
+    }
+
+    public static void stopDriver() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
